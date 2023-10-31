5353672cd22f482cf481b4372a929932655dd27b
Matthew Wong <mattwong991@gmail.com>
2023-10-31 16:36:21 -0600

    environments setup

--- client/src/env.js
+++ client/src/env.js
@@ -1,6 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const domain = 'mwong-bcw.us.auth0.com'
+export const clientId = 'Fw90MqY0RzAH70uzAICHzSh4ZuP3YUwa'
+export const audience = 'https://matt-bcwstudent.com'
